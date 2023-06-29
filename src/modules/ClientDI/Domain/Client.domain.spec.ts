0a76f4ad12d48eb21773821ea15254c384d5d801
NicholasWM <nicholaswm.42@gmail.com>
2023-06-29 20:16:05 -0300

    refact: segmentation of tests

--- src/modules/ClientDI/Domain/Client.domain.spec.ts
+++ src/modules/ClientDI/Domain/Client.domain.spec.ts
@@ -1,9 +1,10 @@
+import mongoose from "mongoose"
 import { GenericRepository } from "../../../common/generic.repository"
 import { GenericInMemoryRepository } from "../../../infra/database/in-memory/generic.repository"
 import { connection } from "../../../server"
 import { ClientController } from "../Controllers/Client.controller"
 import { ClientInMemoryRepository } from "../Repositories/In-Memory/Client.inMemory.implementation"
-import { ClientSchema } from "../Repositories/mongoose/Client.model"
+import { ClientModel, ClientSchema } from "../Repositories/mongoose/Client.model"
 import { ClientMongooseRepository } from "../Repositories/mongoose/Client.repository"
 import { ClientService } from "../Services/Client.service"
 import { ClientDomain, IClientProps } from "./Client.domain"
@@ -16,21 +17,67 @@ const getRepository = () => {
 }
 
 describe('Client Domain', () => {
-    // let clientRepository: ClientMongooseRepository
-    let clientService: ClientService<IClientProps, IClientProps, any>
-    beforeEach(async () => {
-        // const model = (await connection).model('Client', ClientSchema);
-        let clientRepository: GenericRepository
-        // clientRepository = new ClientMongooseRepository(model)
-        clientRepository = new ClientInMemoryRepository()
-        clientService = new ClientService(clientRepository)
+
+    describe('ClientDI In Memory Repository', () => {
+        let clientService: ClientService<IClientProps, IClientProps, any>
+
+        //POSSIBILIDADE DE CONFIGURAR UMA NOVA BASE PARA CADA TESTE
+        // beforeEach(async () => {
+        beforeAll(async () => {
+            let clientRepository: GenericRepository
+            clientRepository = new ClientInMemoryRepository()
+            clientService = new ClientService(clientRepository)
+        })
+        test('Should create a client', async () => {
+            const client = new ClientDomain(
+                'John Doe',
+            )
+        
+            const createdItem = await clientService.create(client)
+            const clientCreated = await clientService.findOne({ name: 'John Doe' })
+            console.log(clientCreated)
+            expect(clientCreated.name).toBe(client.name)
+            expect(client.userId).toBe(undefined)
+        })
+    
+        test('Should update a client', async () => {
+            const client = new ClientDomain(
+                'John Doe',
+            )
+                
+            await clientService.create(client)
+            const clientCreated = await clientService.findOne({ name: 'John Doe' })
+        
+            expect(clientCreated.name).toBe(client.name)
+            expect(client.userId).toBe(undefined)
+    
+            const modified = { name: 'Jane Doe' }
+
+            await clientService.updateById(clientCreated.id, modified)
+
+            const clientUpdated = await clientService.findOne({ name: 'Jane Doe' })
+
+            expect(clientUpdated.name).toBe(modified.name)
+            expect(client.userId).toBe(undefined)
+        })
     })
 
-    // afterEach(() => {
-    //     clientRepository.dropDatabase()
-    // })
+    describe('ClientDI In MongoDB', () => {
+        let clientRepository: ClientMongooseRepository
+        let connectionMongo: typeof mongoose
+        let clientService: ClientService<IClientProps, IClientProps, any>
 
-    describe('Client In Memory Repository', () => {
+        beforeEach(async () => {
+            const connectionMongo = await connection
+            const model = connectionMongo.model('Client', ClientSchema);
+            clientRepository = new ClientMongooseRepository(model)
+            clientService = new ClientService(clientRepository)
+        })
+
+        afterEach(() => {
+            clientRepository.dropDatabase()
+        })
+
         test('Should create a client', async () => {
             const client = new ClientDomain(
                 'John Doe',
